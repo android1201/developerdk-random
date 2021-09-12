b7ce08a9d85b007f79e4a07ed3175a3e04e2adb9
Durgesh Kumar <developerdk1201@gmail.com>
2021-09-12 19:55:31 +0530

    @Author developerdk1973

---
+++ src/functions/checkupdate.js
@@ -0,0 +1,77 @@
+/*
+ * @Author developerdk
+ * @Copyright 2021
+ * @Module export
+ */
+module.exports = {
+	checkupdate
+};
+/*
+ * @Function checkupdate
+ */
+async function checkupdate() {
+	if (!require(`superagent`)) {
+		return;
+	}
+	await require(`superagent`)
+		.get(`https://registry.npmjs.com/${require(`../package.json`).name}`)
+		.end((err, response) => {
+			const packagedata = JSON.parse(response.text);
+			if (!packagedata.error && packagedata[`dist-tags`] != undefined) {
+				if (
+					require(`../package.json`).version !== packagedata[`dist-tags`].latest
+				) {
+					console.log(`\n\n`);
+					console.log(
+						`\x1b[32m` + `---------------------------------------------------`
+					);
+					console.log(
+						`\x1b[32m` +
+						`| @ ${
+								require(`../package.json`).name
+							}                        - [] X |`
+					);
+					console.log(
+						`\x1b[32m` + `---------------------------------------------------`
+					);
+					console.log(
+						`\x1b[33m` +
+						`|            The module is\x1b[31m out of date!\x1b[33m           |`
+					);
+					console.log(
+						`\x1b[35m` + `|             New version is available!           |`
+					);
+					console.log(
+						`\x1b[34m` +
+						`|                  ${require(`../package.json`).version} --> ${
+								packagedata[`dist-tags`].latest
+							}                |`
+					);
+					console.log(
+						`\x1b[36m` +
+						`|        Run "npm i ${
+								require(`../package.json`).name
+							}@latest"       |`
+					);
+					console.log(
+						`\x1b[36m` + `|                    to update!                   |`
+					);
+					console.log(
+						`\x1b[37m` + `|          View the full changelog here:          |`
+					);
+					console.log(
+						`\x1b[31m` +
+						`https://www.npmjs.com/package/${require(`../package.json`).name}`
+					);
+					console.log(
+						`\x1b[32m` +
+						`---------------------------------------------------\x1b[37m`
+					);
+					console.log(`\n\n`);
+				}
+			}
+		});
+}
+/*
+ * @Function done
+ */
---
+++ src/testings/developerdk.js
@@ -0,0 +1,6 @@
+const hubimage = require('../../src/module/module.js').random;
+
+const hub = new hubimage();
+for (let i = 0; i < 10; ++i) {
+    console.log(hub.porngif());
+}
