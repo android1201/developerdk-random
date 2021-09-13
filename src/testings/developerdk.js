98ce2ce0c681c4604d376317c5db890c713e4abc
Durgesh Kumar <89626036+developerdk1973@users.noreply.github.com>
2021-09-13 15:11:19 +0530

    Update developerdk.js

--- src/testings/developerdk.js
+++ src/testings/developerdk.js
@@ -3,12 +3,15 @@
  * @Github developerdk1973
  * @Testing random
  */
-const { random } = require('../../src/module/module.js');
+const {
+	random
+} = require('../../src/module/module.js');
 
 const image = new random();
 for (let i = 0; i < 10; ++i) {
-    console.log(image.porngif());
+	console.log(image.porngif());
 }
+console.log(image.porn.japanese());
 /*
  * @Testing done
  */
