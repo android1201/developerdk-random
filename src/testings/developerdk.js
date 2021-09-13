8d770bfb98aee9be00062fe7095a912a5410724f
Durgesh Kumar <89626036+developerdk1973@users.noreply.github.com>
2021-09-13 18:31:51 +0530

    Api2 full test

--- src/testings/developerdk.js
+++ src/testings/developerdk.js
@@ -11,7 +11,16 @@ const image = new random();
 for (let i = 0; i < 10; ++i) {
 	console.log(image.porngif());
 }
+
 image.nsfw.blowjob().then((R_I) => {
+	console.log(R_I);
+});
+
+image.nsfw1.anal().then((R_I) => {
+	console.log(R_I);
+});
+
+image.sfw1.kemonomimi().then((R_I) => {
 	console.log(R_I);
 });
 /*
